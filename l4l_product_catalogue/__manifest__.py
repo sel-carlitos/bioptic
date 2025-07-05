2e723f11a1f22b40bbe81b3dc30debd03fea3160
Carlitos Melgar <carlitos.melgar@sistemas-en-linea.com>
2025-07-05 23:24:21 +0000

    [FIX] l4l_product_catalogue: remove sequence from manifest

--- l4l_product_catalogue/__manifest__.py
+++ l4l_product_catalogue/__manifest__.py
@@ -9,7 +9,6 @@
     'name': "Generate Product Catalogue",
     'category': 'Extra Tools',
     'version': '18.0.1.0',
-    'sequence': 1,
     'summary': """Product Catalogue, Product, Multiple Products, Odoo Product Catalogue Generator, Products, Customizable Catalogue, Catalogue, Catalogue Styling Options, Multi-Currency Catalogue Printing, Product Catalogue Send By Email, Catalogue Customization, Dynamic Product Catalogue Creation, Catalogue Management, Product Catalogue Design Tool, Website, E-Commerce, Catalogue of Product, Mail, Send By Mail, Product View, Catalogue Templates, Generate Product Catalogue, Send Product Catalogue to Customer, Product Catalogue Style, Various Styles, Catalogue Layout Customization, Catalogue Printing Options, Product Presentation, Product Showcase, L4L, Leap, 4, Logic, Leap4Logic""",
     'description': """The Customizable Product Catalogue Maker in Odoo Helps User to Easily Create and Customize Product Catalogues to Fit Their Needs. It Has a Simple Interface and Lots of Options to Customize. This Makes It Easy to Create Nice-Looking Catalogues to Share with Customers.""",
     'author': 'Leap4Logic Solutions Private Limited',
