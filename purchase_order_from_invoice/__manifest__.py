917fab6a67926c3e49a5bea289c867ab3841734e
Carlitos Melgar <carlitos.melgar@sistemas-en-linea.com>
2025-07-05 23:24:53 +0000

    [FIX] purchase_order_from_invoice: remove sequence from manifest

--- purchase_order_from_invoice/__manifest__.py
+++ purchase_order_from_invoice/__manifest__.py
@@ -4,7 +4,6 @@
     'name': "Create Purchase Order from Invoice",
     'category': 'Accounting/Accounting',
     'version': '18.0.1.0',
-    'sequence': 1,
     'summary': """Create Purchase Order from Invoice, Create Purchase Order in Invoice, Create Purchase Order, Create PO, Purchase Order, Invoice, Account, Purchases, Purchase.""",
     'description': """In Odoo There is no Option to Reverse Process Like User can Create Purchase Order from the Invoice so This Plugin will Help to Create Purchase Order from Customer Invoice.""",
     'author': 'Carlos Melgar',
