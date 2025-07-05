b8344ad66957e3576d1a4eb86e48d5cce28eb8d4
Carlitos Melgar <carlitos.melgar@sistemas-en-linea.com>
2025-07-05 23:25:07 +0000

    [FIX] l4l_stock_back_date: remove sequence from manifest

--- l4l_stock_back_date/__manifest__.py
+++ l4l_stock_back_date/__manifest__.py
@@ -9,7 +9,6 @@
     'name': 'Stock Back Date',
     'category': 'Stock',
     'version': '18.0.1.0',
-    'sequence': 5,
     'summary': 'BackDate in Stock, Set BackDate in Stock, Stock, Sale, Sale Order, Purchase, Purchase Order, Inventory, Transfer, Invoice',
     'description': "The standard Vanilla Odoo does not support backdating transactions. However, with the introduction of this module, users gain the flexibility to modify the scheduled date to a past date. Consequently, all associated documents, such as stock movements and journal entries linked to deliveries or shipments, will reflect the selected date. This enhancement allows for greater adaptability in managing transactional timelines within the Odoo system.                             The Standard Vanilla Odoo does not Support Backdating Transactions. However, with the Introduction of This Module, Users Gain the Flexibility to Modify the Scheduled Date to a Past Date. Consequently, All Associated Documents, Such as Stock Movements and Journal Entries Linked to Deliveries or Shipments, Will Reflect the Selected Date. This Enhancement Allows for Greater Adaptability in Managing Transactional Timelines Within the Odoo System.",
     'author': 'Leap4Logic Solutions Private Limited',
