282a3717660332f4b2686bfad2cc0fde91a9ad81
Carlitos Melgar <carlitos.melgar@sistemas-en-linea.com>
2025-07-06 05:53:13 +0000

    [FIX] l10n_sv_dte: res.partner form
    Changed the `default_get` to set res.partner to the account_fiscal_country_id
    
    Updated the form so that the municipality is before the country.

--- l10n_sv_address/models/res_partner.py
+++ l10n_sv_address/models/res_partner.py
@@ -6,3 +6,18 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     res_municipality_id = fields.Many2one('res.municipality', 'Municipality', domain="[('state_id', '=', state_id)]")
+
+    @api.model
+    def default_get(self, fields_list):
+        """
+        Overrides the default_get method to set country_id to the company's
+        fiscal_country_id when creating a new partner.
+        """
+        res = super().default_get(fields_list)
+
+        company = self.env.company 
+
+        if company and company.account_fiscal_country_id:
+            res['country_id'] = company.account_fiscal_country_id.id
+
+        return res
