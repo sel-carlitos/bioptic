98b9899e58c069714ccb3e79a7553a2e5b766848
Carlitos Melgar <carlitos.melgar@sistemas-en-linea.com>
2025-07-19 11:47:26 -0600

    [UPD] l10n_sv: update to origin

--- l10n_sv/__manifest__.py
+++ l10n_sv/__manifest__.py
@@ -1,15 +1,20 @@
+# -*- coding: utf-8 -*-
+
 {
     'name': 'El Salvador - Accounting',
     'countries': ['sv'],
-    'version': '18.0.1.0.0',
+    "version": "18.0.1.0.0",
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
 This is the base module to manage the accounting chart for El Salvador.
 Agrega una nomenclatura contable para El Salvador. También icluye impuestos.
-Adds accounting chart for El Salvador. It also includes taxes.
-    """,
-    'author': 'SEGU',
+Adds accounting chart for El Salvador. It also includes taxes.""",
+    'author': "SEGU",
     'depends': ['account'],
     'auto_install': ['account'],
+    # 'data': [
+    #     'views/partner_views.xml',
+    #     # 'security/ir.model.access.csv',
+    # ],
     'license': 'LGPL-3',
 }
